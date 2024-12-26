af68d914286b614cfb45e726348960cc25610440
Hongyang Zhou <hyzhou@umich.edu>
2024-12-26 12:31:35 -0500

    Polish the generator

--- src/vdfpy/clustering.py
+++ src/vdfpy/clustering.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
@@ -6,12 +7,29 @@ from sklearn.mixture import GaussianMixture
 from sklearn.decomposition import PCA
 
 
-def cluster(df, method: str = "kmeans", n_clusters: int = 4):
+def cluster(
+    df, method: str = "kmeans", n_clusters: int = 2, *, verbose: bool = True
+) -> np.ndarray:
+    """Clustering input data into n_clusters with a selected method.
+
+    Args:
+        df (_type_): input data in the form of a DataFrame or ndarray.
+        method (str, optional): clustering method. Defaults to "kmeans".
+        n_clusters (int, optional): number of clusters. Defaults to 2.
+        verbose (bool, optional): logging information. Defaults to True.
+
+    Raises:
+        Exception: when the prescribed method is not implemented.
+
+    Returns:
+        np.ndarray: labels of classes as integers.
+    """
     (n_samples, n_features) = df.shape
 
-    print(
-        f"# clusters: {n_clusters}; # samples: {n_samples}; # features {n_features}\n"
-    )
+    if verbose:
+        print(
+            f"# clusters: {n_clusters}; # samples: {n_samples}; # features: {n_features}\n"
+        )
 
     scaler = StandardScaler()
     data_scaled = scaler.fit_transform(df)
--- src/vdfpy/generator/generator.py
+++ src/vdfpy/generator/generator.py
@@ -1,5 +1,7 @@
 # Pseudo distribution function generator.
 
+import random
+import math
 import numpy as np
 import pandas as pd
 
@@ -10,7 +12,8 @@ def make_clusters(
     n_points: int = 100,
     n_dims: int = 1,
     *,
-    random_state: int = None
+    random_state: int = None,
+    shuffle: bool = False
 ) -> pd.DataFrame:
     """Create pseudo-distribution samples for testing the clustering methods.
 
@@ -20,10 +23,12 @@ def make_clusters(
         n_points (int, optional): Number of maximum particles in each sampling. Defaults to 100.
         n_dims (int, optional): Dimensionality. Defaults to 1.
         random_state (int, optional): Seed for the random number generator. Defaults to None.
+        shuffle (bool, optional): Shuffle the sampling order. Defaults to False.
 
     Returns:
         pd.DataFrame: Samples of pseudo-distributions.
     """
+    assert n_samples >= n_clusters, "Cannot generate fewer samples than classes!"
     if random_state is not None:
         rng = np.random.default_rng(random_state)
     else:
@@ -34,12 +39,11 @@ def make_clusters(
         if n_clusters == 1:
             samples = [rng.normal(0, 1, size=u) for u in upper]
         elif n_clusters == 2:
-            ns2 = int(n_samples * 0.5)
-            ns1 = n_samples - ns2
+            ns1 = n_samples - math.ceil(n_samples / 2)
             s1 = [rng.normal(0, 1, size=upper[i]) for i in range(ns1)]
-            s21 = [rng.normal(0, 1, size=upper[i]) for i in range(ns2, n_samples)]
+            s21 = [rng.normal(0, 1, size=upper[i]) for i in range(ns1, n_samples)]
             # Add bump-on-tail species
-            s22 = [rng.normal(4, 1, size=upper[i] // 2) for i in range(ns2, n_samples)]
+            s22 = [rng.normal(4, 1, size=upper[i] // 2) for i in range(ns1, n_samples)]
             s2 = [np.concatenate([arr1, arr2]) for arr1, arr2 in zip(s21, s22)]
             samples = s1 + s2
         elif n_clusters == 3:
@@ -54,26 +58,55 @@ def make_clusters(
             s3 = [np.concatenate([arr1, arr2]) for arr1, arr2 in zip(s31, s32)]
             samples = s1 + s2 + s3
 
-        d1 = pd.Series(list(samples), name="particle velocity")
+        if shuffle:
+            random.shuffle(samples)
+
+        samples = [pd.DataFrame(s, columns=["vx"]) for s in samples]
+        d1 = pd.Series(samples, name="particle velocity")
         d2 = pd.Series([float(s.shape[0]) for s in samples], name="density")
         d3 = pd.Series([np.mean(s) for s in samples], name="bulk velocity")
-        d4 = pd.Series([np.std(s) for s in samples], name="temperature")
+        d4 = pd.Series([np.std(s.values) for s in samples], name="temperature")
         df = pd.concat([d1, d2, d3, d4], axis=1)
     elif n_dims == 2:
-        #TODO Complete the 2D generations!
-        if n_clusters == 2:
+        upper = rng.integers(1, n_points, size=n_samples)
+        if n_clusters == 1:
             mean = [0, 0]
             cov = [[1, 0], [0, 1]]
-            n1 = n_points
-            samples1 = rng.multivariate_normal(mean, cov, size=n1)
+            samples = [rng.multivariate_normal(mean, cov, size=u) for u in upper]
+        elif n_clusters == 2:
+            ns1 = n_samples - math.ceil(n_samples / 2)
 
-            mean = [3, 3]
-            cov = [[1, 0], [0, 1]]
-            n2 = n_points // 10
-            samples2 = rng.multivariate_normal(mean, cov, size=n2)
+            mean1 = [0, 0]
+            cov1 = [[1, 0], [0, 1]]
+            s1 = [
+                rng.multivariate_normal(mean1, cov1, size=upper[i]) for i in range(ns1)
+            ]
 
-            samples = np.concatenate((samples1, samples2))
+            mean2 = [4, 4]
+            cov2 = [[1, 0], [0, 1]]
+            s21 = [
+                rng.multivariate_normal(mean1, cov1, size=upper[i])
+                for i in range(ns1, n_samples)
+            ]
+            s22 = [
+                rng.multivariate_normal(mean2, cov2, size=upper[i] // 2)
+                for i in range(ns1, n_samples)
+            ]
+            s2 = [np.concatenate([arr1, arr2]) for arr1, arr2 in zip(s21, s22)]
 
-        df = pd.DataFrame(samples, columns=["x", "y"])
+            samples = s1 + s2
+
+        if shuffle:
+            random.shuffle(samples)
+
+        samples = [pd.DataFrame(s, columns=["vx", "vy"]) for s in samples]
+        d1 = pd.Series(samples, name="particle velocity")
+        d2 = pd.Series([float(s.shape[0]) for s in samples], name="density")
+        d3 = pd.Series([np.mean(s, axis=0) for s in samples], name="bulk velocity")
+        # scalar temperature
+        d4 = pd.Series(
+            [np.mean(np.std(s, axis=0)) for s in samples], name="temperature"
+        )
+        df = pd.concat([d1, d2, d3, d4], axis=1)
 
     return df
